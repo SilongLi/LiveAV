bdbeecdc770d6b7968e381979329402dbfe8d18f
lisilong <lisilong@tuandai.com>
2018-09-03 10:30:37 +0800

    添加加载提示。

--- LiveAV/ViewController.swift
+++ LiveAV/ViewController.swift
@@ -14,21 +14,17 @@ private let LivePlayerUrl = "http://5815.liveplay.myqcloud.com/live/5815_89aad37
 
 // 点播视屏
 /// 主视频
-private let MainVodPlayerUrl = "http://1257264886.vod2.myqcloud.com/5d693b70vodgzp1257264886/a6931c2f5285890781222549274/ZMRWeesLHZAA.mp4"
+private let MainVodPlayerUrl = "http://1257264886.vod2.myqcloud.com/5d693b70vodgzp1257264886/342ea44e5285890781455239323/AxV6UlAdByAA.mp4"
 /// 插播视频A
 private let VodPlayerUrlA = "http://1257264886.vod2.myqcloud.com/5d693b70vodgzp1257264886/391fbb9b5285890781455476576/F3gYtcgtKoEA.mp4"
 /// 插播视频B
 private let VodPlayerUrlB = "http://1257264886.vod2.myqcloud.com/5d693b70vodgzp1257264886/36cb1afa5285890781455377467/v0Yv0Pkl0fIA.mp4"
 
-private let StartPlayVodATime: Float = 5.0 * 60.0  // 插播视频A时间（单位秒）
-private let StartPlayVodBTime: Float = 9.0 * 60.0  // 插播视频B时间（单位秒）
+private let StartPlayVodATime: Float = 0.5 * 60.0  // 插播视频A时间（单位秒）
+private let StartPlayVodBTime: Float = 2.0 * 60.0  // 插播视频B时间（单位秒）
 
 
-/*
- 1. 如果两个插播视频没有加载出来咋办，需要判断是否能播放，不能播放不插播？？
- 2. 预加载时间点的控制，保证能播放；
- 3.
- */
+private let noteColor = UIColor.init(red: 220.0/255.0, green: 220.0/255.0, blue: 220.0/255.0, alpha: 1.0)
 
 class ViewController: UIViewController {
     
@@ -63,23 +59,45 @@ class ViewController: UIViewController {
         return player
     }()
     
+    lazy var mainNoteLabel: UILabel = { // 主视频提示信息
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = UIColor.green
+        label.textAlignment = .center
+        label.text = "加载中..."
+        return label
+    }()
+    
     let mainView: UIView = {   //  承载直播视频界面
         let view = UIView()
+        view.backgroundColor = noteColor
         return view
     }()
     
     let mainProgress: UIProgressView = {
         let view = UIProgressView.init(progressViewStyle: UIProgressViewStyle.default)
+        view.isHidden = true
         return view
+    }()
+    
+    lazy var noteLabel: UILabel = { // 插播视频提示信息
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = UIColor.green
+        label.textAlignment = .center
+        label.text = "加载中..."
+        return label
     }()
     
     let vodView: UIView = {   //  承载点播视频界面
         let view = UIView()
+        view.backgroundColor = noteColor
         return view
     }()
     
     let vodProgress: UIProgressView = {
         let view = UIProgressView.init(progressViewStyle: UIProgressViewStyle.default)
+        view.isHidden = true
         return view
     }()
     
@@ -104,6 +122,9 @@ class ViewController: UIViewController {
         view.addSubview(mainView)
         mainProgress.frame = CGRect.init(x: 0.0, y: mainView.frame.size.height, width: mainView.frame.size.width, height: 10)
         mainView.addSubview(mainProgress)
+        let noteH: CGFloat = 20.0
+        mainNoteLabel.frame = CGRect.init(x: 0.0, y: (mainView.frame.size.height - noteH) * 0.5, width: mainView.frame.size.width, height: noteH)
+        mainView.addSubview(mainNoteLabel)
         
         vodView.frame =  CGRect.init(x: 0.0, y: 300.00, width: UIScreen.main.bounds.size.width, height: 200)
         vodView.isHidden = true
@@ -111,6 +132,8 @@ class ViewController: UIViewController {
         view.addSubview(vodView)
         vodProgress.frame = CGRect.init(x: 0.0, y: vodView.frame.size.height, width: mainView.frame.size.width, height: 10)
         vodView.addSubview(vodProgress)
+        noteLabel.frame = CGRect.init(x: 0.0, y: (vodView.frame.size.height - noteH) * 0.5, width: vodView.frame.size.width, height: noteH)
+        vodView.addSubview(noteLabel)
     }
     
     // MARK: - actions
@@ -163,15 +186,17 @@ extension ViewController: TXVodPlayListener {
     }
     
     func onPlayEvent(_ player: TXVodPlayer!, event EvtID: Int32, withParam param: [AnyHashable : Any]!) {
+        // 更新加载提示
+        updateNoteLabel(player, event: EvtID)
+        
         /// 主视频
         if player == mainVodPlayer {
-            if (EvtID == PLAY_EVT_PLAY_PROGRESS.rawValue) {
-                let progressStr = String(describing: param[EVT_PLAY_PROGRESS] ?? "0.0")
-                let durationStr = String(describing: param[EVT_PLAY_DURATION] ?? "0.0")
-                let progress = DecimalNumberTool.float(num: progressStr)
-                let duration = DecimalNumberTool.float(num: durationStr)
-                let percent = progress / duration
-                mainProgress.setProgress(percent, animated: true)
+            mainProgress.isHidden = !mainVodPlayer.isPlaying()
+            
+            if (EvtID == PLAY_EVT_PLAY_PROGRESS.rawValue) { // 播放进度
+                let progress = DecimalNumberTool.float(num: String(describing: param[EVT_PLAY_PROGRESS] ?? "0.0"))
+                let duration = DecimalNumberTool.float(num: String(describing: param[EVT_PLAY_DURATION] ?? "0.0"))
+                mainProgress.setProgress(progress / duration, animated: true)
                 
                 // 1. 在即将播放插播视频A的时候，先预加载视频A
                 if progress >= (StartPlayVodATime - 20.0), !hasPrepareVodePlayerA {
@@ -184,6 +209,7 @@ extension ViewController: TXVodPlayListener {
                     !vodPlayerA.isPlaying(),
                     vodView.isHidden,
                     vodPlayerA.playableDuration() > 0.0 {
+                    
                     vodView.isHidden = false
                     vodPlayerA.setupVideoWidget(vodView, insert: 0)
                     vodPlayerA.resume()
@@ -201,6 +227,7 @@ extension ViewController: TXVodPlayListener {
                     !vodPlayerB.isPlaying(),
                     vodView.isHidden,
                     vodPlayerB.playableDuration() > 0.0 {
+                    
                     vodView.isHidden = false
                     vodPlayerB.setupVideoWidget(vodView, insert: 0)
                     vodPlayerB.resume()
@@ -208,22 +235,21 @@ extension ViewController: TXVodPlayListener {
                 }
                 
                 if vodView.isHidden == true {
-                    print("---M-----：\(progress)--\(duration)--\(percent * 100.0)%-------")
+                    print("---M-----：\(progress)--\(duration)--\(progress / duration * 100.0)%-------")
                 }
             }
         }
         
         /// 视频A
         if player == vodPlayerA {
+            vodProgress.isHidden = !vodPlayerA.isPlaying()
+            
             if (EvtID == PLAY_EVT_PLAY_PROGRESS.rawValue) {
-                let progressStr = String(describing: param[EVT_PLAY_PROGRESS] ?? "0.0")
-                let durationStr = String(describing: param[EVT_PLAY_DURATION] ?? "0.0")
-                let progress = DecimalNumberTool.float(num: progressStr)
-                let duration = DecimalNumberTool.float(num: durationStr)
-                let percent = progress / duration
+                let progress = DecimalNumberTool.float(num: String(describing: param[EVT_PLAY_PROGRESS] ?? "0.0"))
+                let duration = DecimalNumberTool.float(num: String(describing: param[EVT_PLAY_DURATION] ?? "0.0"))
                 if vodPlayerA.isPlaying() {
-                    vodProgress.setProgress(percent, animated: true)
-                    print("--A------：\(progress)--\(duration)--\(percent * 100.0)%-------")
+                    vodProgress.setProgress(progress / duration, animated: true)
+                    print("--A------：\(progress)--\(duration)--\(progress / duration * 100.0)%-------")
                 }
             }
             
@@ -238,15 +264,14 @@ extension ViewController: TXVodPlayListener {
         
         /// 视频B
         if player == vodPlayerB {
+            vodProgress.isHidden = !vodPlayerB.isPlaying()
+            
             if (EvtID == PLAY_EVT_PLAY_PROGRESS.rawValue) {
-                let progressStr = String(describing: param[EVT_PLAY_PROGRESS] ?? "0.0")
-                let durationStr = String(describing: param[EVT_PLAY_DURATION] ?? "0.0")
-                let progress = DecimalNumberTool.float(num: progressStr)
-                let duration = DecimalNumberTool.float(num: durationStr)
-                let percent = progress / duration
+                let progress = DecimalNumberTool.float(num: String(describing: param[EVT_PLAY_PROGRESS] ?? "0.0"))
+                let duration = DecimalNumberTool.float(num: String(describing: param[EVT_PLAY_DURATION] ?? "0.0"))
                 if vodPlayerB.isPlaying() {
-                    vodProgress.setProgress(percent, animated: true)
-                    print("--B------：\(progress)--\(duration)--\(percent * 100.0)%-------")
+                    vodProgress.setProgress(progress / duration, animated: true)
+                    print("--B------：\(progress)--\(duration)--\(progress / duration * 100.0)%-------")
                 }
                 
                 if EvtID == PLAY_EVT_PLAY_END.rawValue || (!vodPlayerB.isPlaying() && vodProgress.progress > 0.99) {
@@ -257,6 +282,29 @@ extension ViewController: TXVodPlayListener {
                     print("结束播放视频B")
                 }
             }
+        }
+    }
+    
+    func updateNoteLabel(_ player: TXVodPlayer, event EvtID: Int32) {
+        var note = mainNoteLabel
+        if player == mainVodPlayer {
+        } else {
+            note = noteLabel
+        }
+        if EvtID == PLAY_ERR_NET_DISCONNECT.rawValue {  // 请求资源失败
+            note.text = "加载失败，请稍后重试！"
+            note.isHidden = false
+            note.textColor = .red
+            
+        } else if EvtID == PLAY_EVT_PLAY_BEGIN.rawValue {   // 即将开始
+            note.text = ""
+            note.isHidden = true
+            note.textColor = .green
+            
+        } else if EvtID == PLAY_EVT_PLAY_LOADING.rawValue { // 加载中
+            note.text = "加载中..."
+            note.isHidden = false
+            note.textColor = .green
         }
     }
 }
